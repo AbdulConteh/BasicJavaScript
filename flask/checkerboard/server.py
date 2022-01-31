2394e90db3394e2af280ef9db09a536a84e842b8
AbdulConteh <abdulrahmanwc@gmail.com>
2022-01-30 19:44:21 -0500

    checkerboard assignment

--- flask/checkerboard/server.py
+++ flask/checkerboard/server.py
@@ -4,11 +4,23 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', phrase = 'Check Bate!')
+    return render_template('index.html',row=8,col=8,color_one='red',color_two='black')
 
-@app.route('/checkerboard')
-def play():
-    return render_template('index.html')
+@app.route('/<int:x>')
+def gameboard(x):
+    return render_template('index.html',row=x,col=8,color_one='red',color_two='black')
+
+@app.route('/<int:x>/<int:y>')
+def gameboard_one(x,y):
+    return render_template('index.html',row=x,col=y,color_one='blue',color_two='orange')
+
+@app.route('/<int:x>/<int:y>/<string:one>')
+def gameboard_two(x,y,one):
+    return render_template('index.html',row=x,col=y,color_one=one,color_two='black')
+
+@app.route('/<int:x>/<int:y>/<string:one>/<string:two>')
+def gameboard_three(x,y,one,two):
+    return render_template('index.html',row=x,col=y,color_one=one, color_two=two)
 
 if __name__=="__main__":
     app.run(debug=True)
